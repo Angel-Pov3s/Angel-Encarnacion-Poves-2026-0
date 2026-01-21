bebf1d2ce174374d8d04fbe048bedb0eeed2ef78
Ángel Poves <angel.encarnacion@unas.edu.pe>
2026-01-21 01:32:38 -0500

    Update Sumas.py

--- Sumas.py
+++ Sumas.py
@@ -1,4 +1,6 @@
-metros = float(input("Ingrese la cantidad en metros: "))
-centimetros = metros * 100
-print("La cantidad en centimetros es:", centimetros)
+num1 = float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
 
+producto = num1 * num2
+
+print("El resultado de la multiplicacion es:", producto)
